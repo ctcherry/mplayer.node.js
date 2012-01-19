d0d63e32f7aab6f04a2cb7f3abbfba4b76f0f412
Chris Cherry <ctcherry@gmail.com>
2012-01-18 21:25:09 -0800

    Better signal handling to allow for cleanup Allow setting custom socket file Verbose logging

--- mplayer.node.js
+++ mplayer.node.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 
 var socket_file = "/tmp/mplayer.node.sock";
 
-process.on('exit', function() {
-  fs.unlinkSync(socket_file);
-});
+if (process.argv[2]) {
+  socket_file = process.argv[2];
+}
 
 var mplayer = spawn('mplayer', ['-idle', '-slave']);
 
@@ -14,7 +14,17 @@ mplayer.stdout.on('data', function(data) {
     console.log(data.toString());
 });
 
-var server = net.createServer(function(c) { //'connection' listener
+process.on('exit', function() {
+  console.log('- Shutting down');
+  console.log('- Stopping MPlayer');
+  mplayer.kill('SIGINT');
+  console.log('- Cleaning up socket');
+  fs.unlinkSync(socket_file);
+});
+
+process.on('SIGINT', process.exit);
+
+var server = net.createServer(function(c) {
   console.log('server connected');
   c.on('end', function() {
     console.log('server disconnected');
@@ -25,11 +35,11 @@ var server = net.createServer(function(c) { //'connection' listener
   });
 });
 
-server.listen(socket_file, function() { //'listening' listener
-  console.log(socket_file+' socket open');
+server.listen(socket_file, function() {
+  console.log("- Unix Socket open at: "+socket_file);
   fs.chmod(socket_file, "777");
 });
 
 
-console.log('mplayer child pid: ' + mplayer.pid);
+console.log('- Mplayer child pid: ' + mplayer.pid);
 
