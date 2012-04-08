d7087859c6c3dd6b1dab2f0d272fe9aed47f00fb
Chris Cherry <ctcherry@gmail.com>
2012-04-08 02:22:51 -0700

    Restart mplayer on crash. Clearer logging and better log format. Log to file and STDOUT

--- mplayer.node.js
+++ mplayer.node.js
@@ -2,44 +2,69 @@ var spawn = require('child_process').spawn;
 var net = require('net');
 var fs = require('fs');
 
-var socket_file = "/tmp/mplayer.node.sock";
+var socket_file = "/tmp/mplayerserver.sock";
 
 if (process.argv[2]) {
   socket_file = process.argv[2];
 }
 
-var mplayer = spawn('mplayer', ['-idle', '-slave']);
-
-mplayer.stdout.on('data', function(data) {
-    console.log(data.toString());
-});
+var log_file = "/tmp/mplayerserver.log";
 
 process.on('exit', function() {
-  console.log('- Shutting down');
-  console.log('- Stopping MPlayer');
-  mplayer.kill('SIGINT');
-  console.log('- Cleaning up socket');
   fs.unlinkSync(socket_file);
 });
 
-process.on('SIGINT', process.exit);
+var mplayer;
 
-var server = net.createServer(function(c) {
-  console.log('server connected');
+var load_mplayer = function() {
+  log('server', 'Spawning mplayer...');
+  mplayer = spawn('mplayer', ['-idle', '-slave']);
+  log('server', 'mplayer child pid: ' + mplayer.pid);
+  mplayer.stdout.on('data', function(data) {
+    log('mplayer', data.toString());
+  });
+
+  mplayer.on('exit', load_mplayer);
+}
+
+var logstream = fs.createWriteStream(log_file, {'flags': 'a'});
+
+function timestamp() {
+  var d = new Date();
+  return ''+d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate() + '.' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds(); 
+}
+
+function log(tag, str) {
+  str = str.replace(/^\s+|\s+$/g,'');
+  if (str == "") { return; }
+  var messages = str.split(/(\r?\n)+/);
+  var t = timestamp();
+  for (i in messages) {
+    var message = messages[i].replace(/^\s+|\s+$/g,'');
+    var l = timestamp() +': ['+tag+'] ' + message;
+    logstream.write(l+"\n");
+    console.log(l);
+  }
+}
+
+log('server', 'Starting');
+
+load_mplayer();
+
+var server = net.createServer(function(c) { //'connection' listener
+  log('server', 'client connected');
   c.on('end', function() {
-    console.log('server disconnected');
+    log('server', 'client disconnected');
   });
   c.on('data', function(data) {
-    console.log(data.toString());
-    mplayer.stdin.write(data.toString()+"\n");
+    var m = data.toString();
+    log('client', m);
+    log('server', "Sending command to mplayer: '"+m+"\\n'");
+    mplayer.stdin.write(m+"\n");
   });
 });
 
-server.listen(socket_file, function() {
-  console.log("- Unix Socket open at: "+socket_file);
+server.listen(socket_file, function() { //'listening' listener
+  log('server', "Socket Open: " + socket_file);
   fs.chmod(socket_file, "777");
 });
-
-
-console.log('- Mplayer child pid: ' + mplayer.pid);
-
